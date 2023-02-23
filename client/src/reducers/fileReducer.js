be74dd55c7e7daa8701b860ec8fab92c05d0e7c6
Damian <errollgarner@yandex.ru>
2023-02-23 14:27:33 +0100

    Initialization reducers, store

---
+++ client/src/reducers/fileReducer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+
+}
+
+export const fileReducer = (state = defaultState, action) => {
+    switch (action) {
+
+        default:
+            return state
+    }
+
+}
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+const defaultState = {
+    currentUser: {},
+    isAuth: false
+}
+
+export const userReducer = (state = defaultState, action) => {
+    switch (action) {
+
+        default:
+            return state
+    }
+
+}
