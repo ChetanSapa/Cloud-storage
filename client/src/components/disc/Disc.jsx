94ea5d288d550670ebf2fa059184c1c7a57d5c03
Damian <errollgarner@yandex.ru>
2023-03-23 15:12:34 +0100

    Implementing drug&drop functionality.

--- client/src/components/disc/Disc.jsx
+++ client/src/components/disc/Disc.jsx
@@ -1,4 +1,4 @@
-import {React, useEffect} from 'react';
+import {React, useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux'
 import {getFiles, uploadFile} from '../../actions/file'
 import FileList from './fileList/FileList'
@@ -10,6 +10,7 @@ const Disc = () => {
     const dispatch = useDispatch()
     const currentDir = useSelector(state => state.files.currentDir)
     const dirStack = useSelector(state => state.files.dirStack)
+    const [dragEnter, setDragEnter] = useState(false)
 
     useEffect(() => {
         dispatch(getFiles(currentDir))
@@ -28,21 +29,50 @@ const Disc = () => {
         files.forEach(file => dispatch(uploadFile(file, currentDir)))
     }
 
-    return (
-        <div className={'disc-page'}>
-            <div className={"disc-nav"}>
-                <button className={"disc-btn"} onClick={() => {
-                    backClickHandler()
-                }}>{'<'}</button>
-                <button className="disc-btn" onClick={() => popupHandler()}>Create</button>
-                <div className={'disc-btn'}>
-                    <label htmlFor="uploadFile">Upload file</label>
-                    <input multiple={true} onChange={(event) => fileUploadHandler(event)} type="file" id={'uploadFile'}/>
+    const drugEnterHandler = (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        setDragEnter(true)
+    }
+    const drugLeaveHandler = (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        setDragEnter(false)
+    }
+    const dropHandler = (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        let files = [...event.dataTransfer.files]
+        files.forEach(file => dispatch(uploadFile(file, currentDir)))
+        setDragEnter(false)
+    }
+    return (!dragEnter ?
+            <div className={'disc-page'}
+                 onDragEnter={drugEnterHandler}
+                 onDragLeave={drugLeaveHandler}
+                 onDragOver={drugEnterHandler}>
+                <div className={"disc-nav"}>
+                    <button className={"disc-btn"} onClick={() => {
+                        backClickHandler()
+                    }}>{'<'}</button>
+                    <button className="disc-btn" onClick={() => popupHandler()}>Create</button>
+                    <div className={'disc-btn'}>
+                        <label htmlFor="uploadFile">Upload file</label>
+                        <input multiple={true} onChange={(event) => fileUploadHandler(event)} type="file"
+                               id={'uploadFile'}/>
+                    </div>
                 </div>
+                <FileList/>
+                <Popup/>
             </div>
-            <FileList/>
-            <Popup/>
-        </div>
+            :
+            <div className={'drop-area'}
+                 onDrop={dropHandler}
+                 onDragEnter={drugEnterHandler}
+                 onDragLeave={drugLeaveHandler}
+                 onDragOver={drugEnterHandler}>
+                Drop file here
+            </div>
     );
 };
 
--- client/src/components/disc/fileList/file/File.jsx
+++ client/src/components/disc/fileList/file/File.jsx
@@ -19,11 +19,11 @@ const File = ({file}) => {
         <div className={'file'}>
             <div className="file-title">
                 <img src={file.type === 'dir' ? folderImg : fileImg} alt={file.name}/>
-                <div className="file-title-name" onClick={() => openDirHandler(file)}>{file.name}</div>
+                <div className={file.type === 'dir' ? 'file-title-name-dir': 'file-title-name-file'} onClick={() => openDirHandler(file)}>{file.name}</div>
             </div>
             <div className="file-info">
                 <div className="file-info-date">{file.date.slice(0, 10)}</div>
-                <div className="file-info-size">{file.size} bit</div>
+                <div className="file-info-size">{Math.floor(file.size/1000)} KB</div>
             </div>
         </div>
     );
