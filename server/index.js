83d3682ccfe3015690e8d26a93b310b74b201535
Damian <errollgarner@yandex.ru>
2023-04-26 10:26:01 +0200

    Final fix before deploy

--- server/index.js
+++ server/index.js
@@ -8,27 +8,28 @@ const app = express()
 const PORT = process.env.PORT || config.get('serverPort')
 const cors = require('cors')
 const filePathMiddleware = require('./middleware/filePath.middleware')
-const staticPathMiddleware = require('./middleware/staticPath.middleware')
+// const staticPathMiddleware = require('./middleware/staticPath.middleware')
 const path = require('path')
 
 app.use(cors())
 app.use(fileUpload({}))
 app.use(filePathMiddleware(path.resolve(__dirname, 'files')))
-app.use(staticPathMiddleware(path.resolve(__dirname, 'static')))
+// app.use(staticPathMiddleware(path.resolve(__dirname, 'static')))
 app.use(express.json())
 app.use(express.static('static'))
 app.use('/api/auth', authRouter)
 app.use('/api/files', fileRouter)
-
 const start = async () => {
     try {
+        mongoose.set("strictQuery", false);
         await mongoose.connect(config.get("dbUrl"))
 
         app.listen(PORT, ()=>{
             console.log('Server started on port ', PORT)
         })
     } catch (e) {
-
+        console.error(e.message);
+        process.exit(1);
     }
 }
 
